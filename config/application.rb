37d791cd2f08b5314db60ebe61cc3f5dd25bc94f
Yasuda Toshiyuki <toshi1973814@gmail.com>
2017-09-11 09:35:46 +0000

    add Product

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,8 @@ module Depot
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # config.generators do |g|
-    #   g.test_framework :rspec
-    # end
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
